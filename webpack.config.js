2b442c47ba27810a893b8e264cd0b6b72da55a24
15gym <zz05z@nate.com>
2019-12-11 21:51:44 +0900

    First project upload

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+module.exports = {
+    entry: path.join(__dirname, "src/index.js"),
+    output: {
+	path: path.join(__dirname, "src"),
+	filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "src")
+    }
+}
